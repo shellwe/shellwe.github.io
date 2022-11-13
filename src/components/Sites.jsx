88ba5abf7aad73c5f5c13b2efbfe1f4ddc2d0e2c
Shawn Hellwege <shellwe@gmail.com>
2022-11-13 16:59:45 -0600

    fixed ID error

--- src/components/Sites.jsx
+++ src/components/Sites.jsx
@@ -10,7 +10,7 @@ const Sites = (props) => {
         shortTItle={'CoCreate'}
         phone={"iphone-left-cocreate"}
         mac={"imac-right-cocreate"}
-        description={[<ul><li>Utilized Lighthouse and internal audit tools to increase site performance and validation</li><li>Utilized Drupal to increase the user base who can edit content</li><li>Broke down a complex site to create managable and testable segments</li><li>Site needed to be versatile to support different content layouts</li><li>Had to built the site in a way that would allow expansion and new features without breaking existing content</li><li>Worked with University Communications (UComm) to improve brand expression</li><li>Set up GIt repository so UComm could collaborate on SCSS and JS changes with our department seamlessly</li></ul>]}>
+        description={<ul><li>Utilized Lighthouse and internal audit tools to increase site performance and validation</li><li>Utilized Drupal to increase the user base who can edit content</li><li>Broke down a complex site to create managable and testable segments</li><li>Site needed to be versatile to support different content layouts</li><li>Had to built the site in a way that would allow expansion and new features without breaking existing content</li><li>Worked with University Communications (UComm) to improve brand expression</li><li>Set up GIt repository so UComm could collaborate on SCSS and JS changes with our department seamlessly</li></ul>}>
         <h2><strong>Client:</strong> University of Nebraska–Lincoln</h2>
         <p><strong>Site:</strong> <a href="https://cocreate.unl.edu/" target="_blank" rel="noreferrer">https://cocreate.unl.edu/</a></p>
         <strong>Technologies used:</strong><span className="products">
@@ -33,7 +33,7 @@ const Sites = (props) => {
         shortTItle={'WhyNebraska'}
         phone={"iphone-right-whyneb"}
         mac={"macbook-left-whyneb"}
-        description={[<ul><li>Utilize creative uses for CSS Grid</li><li>Triggered animation and other events at certian scroll heights</li><li>Explored animation and how it affects accessibility</li><li>2-column layout for subpages was incredibly complex and required careful coding practices to prevent it from breaking</li><li>Had to manage multiple viewports and make sure content lays out well</li><li>Colaborated with designer to improve the design.</li><li>Reduced page load size by utilizing picture tag to have multiple images in different sizes/formats</li></ul>]}>
+        description={<ul><li>Utilize creative uses for CSS Grid</li><li>Triggered animation and other events at certian scroll heights</li><li>Explored animation and how it affects accessibility</li><li>2-column layout for subpages was incredibly complex and required careful coding practices to prevent it from breaking</li><li>Had to manage multiple viewports and make sure content lays out well</li><li>Colaborated with designer to improve the design.</li><li>Reduced page load size by utilizing picture tag to have multiple images in different sizes/formats</li></ul>}>
         <h2><strong>Client:</strong> University of Nebraska–Lincoln</h2>
         <p><strong>Site:</strong> <a href="https://admissions.unl.edu/why-nebraska/" target="_blank" rel="noreferrer">https://admissions.unl.edu/why-nebraska/</a></p>
         <strong>Technologies used:</strong><span className="products">
